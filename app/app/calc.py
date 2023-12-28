4218dfe7fa633c384954e77413bf04416509bc3b
Ham12-3 <mobolaji2309@gmail.com>
2023-12-28 20:45:20 +0000

    'update'

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     """Add x an y and retirn the results"""
     return x+y
+
+
+def subtract(x, y):
+    """Subtract x from y and return the results."""
+    return y-x
